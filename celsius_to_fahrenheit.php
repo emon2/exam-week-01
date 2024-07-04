a968a30fa3f05e7040b5b0721d673a2f340145c7
Mosaddequr Rahman Khan <emon2020@outlook.com>
2024-07-04 18:17:27 +0600

    PHP LARAVEL B4 EXAM WEEK ASSIGNMENT FILES

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,8 @@
+<?php
+
+$cel = 32;
+$far = ($cel * 9 / 5) + 32;
+printf("The temperature %.2f degrees Celsius is %.2f degrees Fahrenheit", $cel, $far);
+// another way
+// echo number_format($far, 2);
+?> 
